a316377b73c1e7068b67a2d7fb1a818cc05790de
MSHANTH <miroonshanth123@gmail.com>
2022-10-15 11:02:13 +0530

    Read Me

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ import WelcomePage from './src/Screens/WelcomePage/WelcomePage'
 import AccountCreat from './src/Screens/AccountCreatPage/AccountCreat'
 import DashBoard from './src/Screens/DashBoardPage/DashBoard'
 import AddCarDetails from './src/Screens/AddCarDetailPage/AddCarDetails'
-
-
+import { NavigationContainer } from '@react-navigation/native'
+import { Stack } from 'native-base'
+import { createStackNavigator } from '@react-navigation/stack'
 
 
 
@@ -14,14 +15,28 @@ import AddCarDetails from './src/Screens/AddCarDetailPage/AddCarDetails'
 export default function App() {
   return (
      //<LoginPage/>
-    //<WelcomePage/>
-    //<AccountCreat/>
-    <DashBoard/>
+    <WelcomePage/>
+   //<AccountCreat/>
+    //<DashBoard/>
     // <View>
     //   <Text>
     //     Miroon
     //   </Text>
     // </View>
     //<AddCarDetails/>
+
+    // <NavigationContainer>
+
+
+    //     <Stack.Navigator>
+    //   <Stack.Screen name="WelcomePage" component={WelcomePage} />
+    //   <Stack.Screen name="LoginPage" component={LoginPage} />
+    //   <Stack.Screen name="AccountCreat" component={AccountCreat} />
+    //   <Stack.Screen name="DashBoard" component={DashBoard} />
+    //   <Stack.Screen name="AddCarDetails" component={AddCarDetails} />
+
+    // </Stack.Navigator>
+    // </NavigationContainer>
+
   )
   }
---
+++ README.md
@@ -0,0 +1,22 @@
+Sell and Buy Used Cars
+
+
+
+
+> ###**WelcomePage**
+
+![image of Screen](src/Screens/LogInPage/pic/LogApp.jpg))
+
+> ###**AccountCreat**
+
+![image of Screen](src/Screens/LogInPage/pic/AccCreApp.jpg)
+
+> ###**DashBoard**
+
+![image of Screen](src/Screens/LogInPage/pic/DHBApp.jpg)
+
+> ###**AddCarDetails**
+
+![image of Screen](src/Screens/LogInPage/pic/AddCarApp.jpg)
+
+
